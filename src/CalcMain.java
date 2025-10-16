0e133a16f3e871081f19b980645ba30ea4e1106e
Belén <belenm.fernandez@estudiante.uam.es>
2025-10-16 10:19:25 +0200

    calculator code

---
+++ src/CalcMain.java
@@ -0,0 +1,8 @@
+package calculator;
+
+public class CalcMain {
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+		System.out.println("Solution: " + c.add(30, 12));
+    }
+}
